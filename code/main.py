78d8ee9749f1ea6cadc13ef2b20f515b5a956690
Rod <rodolfo.troncoso@alumnos.usm.cl>
2018-11-28 17:50:06 -0300

    Cleaning code

--- code/main.py
+++ code/main.py
@@ -5,11 +5,23 @@ import sys
 from web3 import Web3
 
 
-parser = argparse.ArgumentParser(description='A Python snipet that looks for the BlockHash and the TxHash, that belongs to a contract address.')
-requiredNamed = parser.add_argument_group('Required named arguments')
-requiredNamed.add_argument('contractAddress', metavar='ContractAddress', type= str, nargs=1, help='Here, you must provide the contract address')
-requiredNamed.add_argument('-p', '--host', help='HTTP provider like: https://mainnet.infura.io/<API_SECRET>', required=True)
-args = parser.parse_args()
+
+
+def getRightBlock(provider, address, currentBlockNumber):
+    return len(provider.eth.getCode(address, currentBlockNumber))
+    
+
+def addressCalculator(provider, currentBlockNumber, attribute):
+    for u in provider.eth.getBlock(currentBlockNumber, True)[attribute]:
+        if u['to'] == None:
+            sender = bytes.fromhex(str(u['from']).replace('0x',''))
+            currentContract = '0x' + str(sha3.keccak_256(rlp.encode([sender, u['nonce']])).hexdigest()[-40:])
+            currentContract = provider.toChecksumAddress(currentContract)
+            return currentContract, u
+
+def outputData(provider, index):
+    sys.stdout.write('Block: ' + str(provider.toHex(index['blockHash'])) + '\n')
+    sys.stdout.write('Transaction: ' + str(provider.toHex(index['hash'])) + '\n')
 
 
 def lookForBlockAndHashBlock(end_point, address):
@@ -18,22 +30,26 @@ def lookForBlockAndHashBlock(end_point, address):
     if web3.isConnected() == True:
         address = web3.toChecksumAddress(address)
         find = 0
-        for i in range(web3.eth.blockNumber):
+        for i in range(4203584, web3.eth.blockNumber):
             if find > 0:
                 break
-                '''Below, according to what I've understood, is the implemented the Connor's suggestion'''
-            elif len(web3.eth.getCode(address, i )) > 0:
-                for u in web3.eth.getBlock(i, True)['transactions']:
-                    if u['to'] == None:
-                        sender1 = bytes.fromhex(str(u['from']).replace('0x',''))
-                        currentContract = '0x' + str(sha3.keccak_256(rlp.encode([sender1, u['nonce']])).hexdigest()[-40:])
-                        currentContract = web3.toChecksumAddress(currentContract)
-                        if currentContract == address:
-                            sys.stdout.write('Block: ' + str(web3.toHex(u['blockHash'])) + '\n')
-                            sys.stdout.write('Transaction: ' + str(web3.toHex(u['hash'])) + '\n')
-                            find +=1
-                            break
+            elif getRightBlock(web3, address, i) > 0:
+                currentContract, index = addressCalculator(web3, i, 'transactions')
+                if currentContract == address:
+                    outputData(web3, index)
+                    find +=1
+                    break
     else:
         sys.stdout.write('There is a problem with the conection' + '\n')
+
+'''
+        Below, is the argument parser
+                                        '''
+
+parser = argparse.ArgumentParser(description='A Python snipet that looks for the BlockHash and the TxHash, that belongs to a contract address.')
+requiredNamed = parser.add_argument_group('Required named arguments')
+requiredNamed.add_argument('contractAddress', metavar='ContractAddress', type= str, nargs=1, help='Here, you must provide the contract address')
+requiredNamed.add_argument('-p', '--host', help='HTTP provider like: https://mainnet.infura.io/<API_SECRET>', required=True)
+args = parser.parse_args()
 
 lookForBlockAndHashBlock(args.host,sys.argv[1])
